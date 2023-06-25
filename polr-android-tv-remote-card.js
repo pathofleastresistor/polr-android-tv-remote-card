f7f28d8bd039f9ee5f6ee95e490130280f4a3a2d
Amit V <amit@amitvaria.com>
2023-06-25 14:03:56 -0700

    update js

--- polr-android-tv-remote-card.js
+++ polr-android-tv-remote-card.js
@@ -468,12 +468,12 @@ __decorate([
 customElements.define("polr-headercard", PoLRHeaderCard);
 
 class PoLRRemoteButton extends s {
-    constructor() {
-        super();
-    }
     render() {
-        return x ` <div id="remotebutton">${this.icon}</div> `;
+        return x `
+            <div @click=${this._click} id="remotebutton">${this.icon}</div>
+        `;
     }
+    _click(ev) { }
 }
 PoLRRemoteButton.styles = i$3 `
         #remotebutton {
@@ -502,6 +502,9 @@ __decorate([
 __decorate([
     n$1()
 ], PoLRRemoteButton.prototype, "icon", void 0);
+__decorate([
+    i("#remotebutton")
+], PoLRRemoteButton.prototype, "remotebutton", void 0);
 customElements.define("polr-remotebutton", PoLRRemoteButton);
 
 /*! Hammer.JS - v2.0.7 - 2016-04-22
@@ -3163,18 +3166,18 @@ class PoLRTouchpad extends s {
         this._mc.on("panstart panmove", (event) => {
             const rect = this.pad.getBoundingClientRect();
             const size = 20;
-            this.circle.style.padding = `${size}px`;
-            this.circle.style.opacity = 1;
-            this.circle.style.transition = "opacity 0s";
             const clientX = event.center.x;
             const clientY = event.center.y;
+            this.circle.style.padding = `${size}px`;
+            this.circle.style.transition = "opacity 0s";
+            this.circle.style.opacity = 1;
             this.circle.style.visibility = "visible";
-            this.circle.style.left = `${this.clamp(clientX - rect.left - size, 0 - size, rect.width - rect.left + size)}px`;
+            this.circle.style.left = `${this.clamp(clientX - rect.left - size, 0 - size, rect.width + size - rect.left)}px`;
             this.circle.style.top = `${this.clamp(clientY - rect.top - size, 0 - size, rect.height - size)}px`;
         });
         this._mc.on("panend", (event) => {
-            this.circle.style.transition = "all 1s";
             this.circle.style.opacity = 0;
+            this.circle.style.transition = "opacity 0.4s";
         });
     }
     disconnectedCallback() {
@@ -3186,10 +3189,20 @@ class PoLRTouchpad extends s {
     }
     render() {
         return x `
-            <div id="pad">
+            <div id="pad" @click=${this._click}>
                 <div id="circle"></div>
             </div>
         `;
+    }
+    _click(e) {
+        var d = document.createElement("div");
+        d.className = "clickEffect";
+        d.style.top = e.clientY + "px";
+        d.style.left = e.clientX + "px";
+        this.pad.appendChild(d);
+        d.addEventListener("animationend", function () {
+            this.pad.removeChild(d);
+        }.bind(this));
     }
     clamp(value, min, max) {
         return Math.min(Math.max(value, min), max);
@@ -3214,6 +3227,27 @@ PoLRTouchpad.styles = i$3 `
             background-color: #1e0d40;
             border-radius: 50%;
             opacity: 0%;
+        }
+
+        div.clickEffect {
+            position: fixed;
+            background-color: #1e0d40;
+            border-radius: 50%;
+            animation: clickEffect 0.4s ease-out;
+        }
+        @keyframes clickEffect {
+            0% {
+                opacity: 1;
+                width: 20px;
+                height: 20px;
+            }
+            100% {
+                opacity: 0.2;
+                width: 15em;
+                height: 15em;
+                margin: -7.5em;
+                border-width: 0.03em;
+            }
         }
     `;
 __decorate([
